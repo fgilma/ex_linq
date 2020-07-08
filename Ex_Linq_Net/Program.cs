571dc6140d9c32883d9ac6f34821c9e04899aaac
Francisco Gil Mayo <francisco.gil.mayo@gmail.com>
2020-07-08 10:32:54 +0200

    Añadido lambdas

--- Ex_Linq_Net/Program.cs
+++ Ex_Linq_Net/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Ex_Linq_Net
 {
@@ -18,6 +19,15 @@ namespace Ex_Linq_Net
                            select num;
             Console.WriteLine("Los numeros pares son:");
             foreach (int num in consulta1)
+            {
+                Console.Write(num + " ");
+            }
+
+            // con lambdas
+            
+            var consulta1L = series.Where(x => x % 2 == 0);
+            Console.WriteLine("\nLambdas: Los numeros pares son:");
+            foreach (int num in consulta1L)
             {
                 Console.Write(num + " ");
             }
@@ -41,7 +51,7 @@ namespace Ex_Linq_Net
             var consulta4 = from num in series
                             where num > 5
                             select num;
-
+            
            
             Console.WriteLine("\nLos numeros menores que 5 son:");
             
@@ -54,6 +64,16 @@ namespace Ex_Linq_Net
             foreach (var a in consulta4)
             {
                 Console.Write(a+ " ");
+            }
+
+            //con lambdas
+            var consulta4L = series.Where(x => x > 5);
+
+            Console.WriteLine("\nLamdbas: Los numeros mayores que 5 son:");
+
+            foreach (var a in consulta4L)
+            {
+                Console.Write(a + " ");
             }
 
             // Fase 4:
@@ -70,6 +90,15 @@ namespace Ex_Linq_Net
                 Console.WriteLine(i);
             }
 
+            //con lambdas
+
+            var consulta5L = serieNombres.Where(x => x[0] == 'O');
+            Console.WriteLine("\n\n Lambdas:Los nombres que empiezan por O son:");
+            foreach (var i in consulta5L)
+            {
+                Console.WriteLine(i);
+            }
+
             //nombres con mas de 6 letras
             var consulta6 = from a in serieNombres
                             where a.Length >= 6
@@ -81,6 +110,15 @@ namespace Ex_Linq_Net
                 Console.WriteLine(i);
             }
 
+            //con lambdas
+            var consulta6L = serieNombres.Where(x => x.Length >= 6);
+
+            Console.WriteLine("\nLambdas: Los nombres que tienen mas de 6 letras son:");
+            foreach (var i in consulta6L)
+            {
+                Console.WriteLine(i);
+            }
+
             //nombres ordenados de forma descendente
             var consulta7 = from a in serieNombres
                             orderby a descending
@@ -88,6 +126,14 @@ namespace Ex_Linq_Net
 
             Console.WriteLine("\nLos nombres ordenados de forma descendiente son:");
             foreach (var i in consulta7)
+            {
+                Console.Write(i + " ");
+            }
+            //con lambdas
+            var consulta7L = serieNombres.OrderByDescending(x=>x);
+
+            Console.WriteLine("\n\nLambdas: Los nombres ordenados de forma descendiente son:");
+            foreach (var i in consulta7L)
             {
                 Console.Write(i + " ");
             }
